0671cd608c7a6fe334827fea4b5618fe4698b4fc
Joey Whalen <jtktwhalen@gmail.com>
2021-09-04 18:50:43 -0400

    Project folder structure.

---
+++ comparev/src/main/java/org/wcci/transportationev/project/comparev/ComparevApplication.java
@@ -0,0 +1,13 @@
+package org.wcci.transportationev.project.comparev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComparevApplication.class, args);
+	}
+
+}
---
+++ comparev/src/test/java/org/wcci/transportationev/project/comparev/ComparevApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wcci.transportationev.project.comparev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComparevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
