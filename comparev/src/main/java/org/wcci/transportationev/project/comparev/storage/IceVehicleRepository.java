127db1f1eaf4d499b55eed3fba0df3bc3028a9e4
Joey Whalen <jtktwhalen@gmail.com>
2021-09-04 20:19:29 -0400

    Created IceVehicleRepository and IceVehicleStorage

---
+++ comparev/src/main/java/org/wcci/transportationev/project/comparev/storage/IceVehicleRepository.java
@@ -0,0 +1,8 @@
+package org.wcci.transportationev.project.comparev.storage;
+
+import org.springframework.data.repository.CrudRepository;
+import org.wcci.transportationev.project.comparev.resources.IceVehicle;
+
+public interface IceVehicleRepository extends CrudRepository<IceVehicle, Long> {
+
+}
