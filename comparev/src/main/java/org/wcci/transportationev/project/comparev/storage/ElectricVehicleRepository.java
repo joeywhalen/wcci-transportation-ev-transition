5becaae8300ccb275403db4b1099c003e1a2e05b
Joey Whalen <jtktwhalen@gmail.com>
2021-09-04 20:13:36 -0400

    Created ElectricVehicleRepository and ElectricVehicleStorage

---
+++ comparev/src/main/java/org/wcci/transportationev/project/comparev/storage/ElectricVehicleRepository.java
@@ -0,0 +1,8 @@
+package org.wcci.transportationev.project.comparev.storage;
+
+import org.springframework.data.repository.CrudRepository;
+import org.wcci.transportationev.project.comparev.resources.ElectricVehicle;
+
+public interface ElectricVehicleRepository extends CrudRepository<ElectricVehicle, Long> {
+
+}
