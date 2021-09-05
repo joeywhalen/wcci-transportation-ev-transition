aafcb1aa5cad803ebf1ead7dc0e55c2d05a18c05
Joey Whalen <jtktwhalen@gmail.com>
2021-09-04 20:04:38 -0400

    Created ArticleTopicRepository and ArticleTopicStorage

---
+++ comparev/src/main/java/org/wcci/transportationev/project/comparev/storage/ArticleTopicRepository.java
@@ -0,0 +1,8 @@
+package org.wcci.transportationev.project.comparev.storage;
+
+import org.springframework.data.repository.CrudRepository;
+import org.wcci.transportationev.project.comparev.resources.ArticleTopic;
+
+public interface ArticleTopicRepository extends CrudRepository<ArticleTopic, Long> {
+
+}
